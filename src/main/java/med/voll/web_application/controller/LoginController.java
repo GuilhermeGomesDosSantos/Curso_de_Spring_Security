5cddfbabddefd67913060cbf1d9600976054460d
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2026-02-01 22:24:39 -0300

    Utilizando os filtros do Spring Security

--- src/main/java/med/voll/web_application/controller/LoginController.java
+++ src/main/java/med/voll/web_application/controller/LoginController.java
@@ -1,2 +1,12 @@
-package med.voll.web_application.controller;public class LoginController {
+package med.voll.web_application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String carregaPaginaListagem(){
+        return "autenticacao/login";
+    }
 }
--- src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
+++ src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -1,2 +1,43 @@
-package med.voll.web_application.infra.security;public class ConfiguracoesSeguranca {
+package med.voll.web_application.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ConfiguracoesSeguranca {
+
+    @Bean
+    public UserDetailsService dadosUsuariosCadastrados(){
+        UserDetails  usuario1 = User.builder()
+                .username("guilherme.santos@email.com")
+                .password("{noop}123456")
+                .build();
+
+        UserDetails usuario2 = User.builder()
+                .username("teste.silva@email.com")
+                .password("{noop}123456")
+                .build();
+
+        return new InMemoryUserDetailsManager(usuario1, usuario2);
+    }
+
+    @Bean
+    public SecurityFilterChain filtrosSeguranca (HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests(req -> {
+            req.requestMatchers("/css/**", "/js/**", "/assets/**").permitAll();
+            req.anyRequest().authenticated();
+        })
+                .formLogin(form -> form.loginPage("/login")
+                .defaultSuccessUrl("/")
+                .permitAll())
+                .build();
+    }
 }
